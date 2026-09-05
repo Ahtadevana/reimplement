fd7a5875f1cc7c2a7738b6278024ab36701c6921
ahtadevana <ahtaullahakmali@gmail.com>
2026-09-05 11:44:20 +0700

    define constructors, destructor and getters

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -2,16 +2,24 @@
 
 template <typename T> class Vector {
 public:
-  Vector();
-  Vector(int count);
-  Vector(int count, T &content);
+  Vector() : data_(nullptr), size_(0), capacity_(0) {}
 
-  ~Vector();
+  Vector(int count) : data_(new T[count]), size_(count), capacity_(count) {}
 
-  [[nodiscard]] bool isAlloc() const;
-  [[nodiscard]] bool isEmpty() const;
-  [[nodiscard]] std::size_t size() const;
-  [[nodiscard]] std::size_t capacity() const;
+  Vector(int count, T &content) : size_(count), capacity_(count) {
+    data_ = new T[count];
+
+    for (std::size_t i{}; i < size_; ++i) {
+      data_[i] = content;
+    }
+  }
+
+  ~Vector() { delete[] data_; }
+
+  [[nodiscard]] bool isAlloc() const { return data_ != nullptr; }
+  [[nodiscard]] bool isEmpty() const { return size_ == 0; }
+  [[nodiscard]] std::size_t size() const { return size_; }
+  [[nodiscard]] std::size_t capacity() const { return capacity_; }
 
 private:
   T *data_;
